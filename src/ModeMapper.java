9e9566eedabdfb82e9672ff2437e2d5b90b1a8c2
m-m-black <morganblack1988@gmail.com>
2019-09-07 14:37:50 +1000

    Added ModeMapper class to map degrees to MIDI note values for each mode

---
+++ src/ModeMapper.java
@@ -0,0 +1,121 @@
+import java.util.HashMap;
+
+public class ModeMapper {
+
+    private HashMap<Degree, Integer> map;
+    private Mode mode;
+
+    public HashMap<Degree, Integer> getMap(Mode mode) {
+        map = null;
+        switch (mode) {
+            case IONIAN:
+                map = getIonian();
+                break;
+            case DORIAN:
+                map = getDorian();
+                break;
+            case PHRYGIAN:
+                map = getPhrygian();
+                break;
+            case LYDIAN:
+                map = getLydian();
+                break;
+            case MIXOLYDIAN:
+                map = getMixolydian();
+                break;
+            case AEOLIAN:
+                map = getAeolian();
+                break;
+            case LOCRIAN:
+                map = getLocrian();
+                break;
+            default:
+                break;
+        }
+        return map;
+    }
+
+    private HashMap<Degree, Integer> getIonian() {
+        map = new HashMap<>();
+        map.put(Degree.I, 60);
+        map.put(Degree.II, 62);
+        map.put(Degree.III, 64);
+        map.put(Degree.IV, 65);
+        map.put(Degree.V, 67);
+        map.put(Degree.VI, 69);
+        map.put(Degree.VII, 71);
+        return map;
+    }
+
+    private HashMap<Degree, Integer> getDorian() {
+        map = new HashMap<>();
+        map.put(Degree.I, 60);
+        map.put(Degree.II, 62);
+        map.put(Degree.III, 63);
+        map.put(Degree.IV, 65);
+        map.put(Degree.V, 67);
+        map.put(Degree.VI, 69);
+        map.put(Degree.VII, 70);
+        return map;
+    }
+
+    private HashMap<Degree, Integer> getPhrygian() {
+        map = new HashMap<>();
+        map.put(Degree.I, 60);
+        map.put(Degree.II, 61);
+        map.put(Degree.III, 63);
+        map.put(Degree.IV, 65);
+        map.put(Degree.V, 67);
+        map.put(Degree.VI, 68);
+        map.put(Degree.VII, 70);
+        return map;
+    }
+
+    private HashMap<Degree, Integer> getLydian() {
+        map = new HashMap<>();
+        map.put(Degree.I, 60);
+        map.put(Degree.II, 62);
+        map.put(Degree.III, 64);
+        map.put(Degree.IV, 66);
+        map.put(Degree.V, 67);
+        map.put(Degree.VI, 69);
+        map.put(Degree.VII, 71);
+        return map;
+    }
+
+    private HashMap<Degree, Integer> getMixolydian() {
+        map = new HashMap<>();
+        map.put(Degree.I, 60);
+        map.put(Degree.II, 62);
+        map.put(Degree.III, 64);
+        map.put(Degree.IV, 65);
+        map.put(Degree.V, 67);
+        map.put(Degree.VI, 69);
+        map.put(Degree.VII, 70);
+        return map;
+    }
+
+    private HashMap<Degree, Integer> getAeolian() {
+        map = new HashMap<>();
+        map.put(Degree.I, 60);
+        map.put(Degree.II, 62);
+        map.put(Degree.III, 63);
+        map.put(Degree.IV, 65);
+        map.put(Degree.V, 67);
+        map.put(Degree.VI, 68);
+        map.put(Degree.VII, 70);
+        return map;
+    }
+
+    private HashMap<Degree, Integer> getLocrian() {
+        map = new HashMap<>();
+        map.put(Degree.I, 60);
+        map.put(Degree.II, 61);
+        map.put(Degree.III, 63);
+        map.put(Degree.IV, 65);
+        map.put(Degree.V, 66);
+        map.put(Degree.VI, 68);
+        map.put(Degree.VII, 70);
+        return map;
+    }
+}
