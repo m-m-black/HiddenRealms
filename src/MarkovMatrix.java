d4caf300735b2a2377765a60a02436a082f10d4b
m-m-black <morganblack1988@gmail.com>
2019-09-07 15:20:28 +1000

    Added MarkovMatrix and Tuple classes

---
+++ src/MarkovMatrix.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class MarkovMatrix {
+
+    private HashMap<Degree, List<Tuple>> matrix;
+    private Degree currentNote;
+
+    public void initMatrix() {
+        /*
+            Randomly generate transition probability values
+            and build matrix
+         */
+    }
+
+    public Degree getNextNote() {
+        /*
+            Generate next note using probability
+         */
+        /*
+            Set currentNote to the new note that was generated
+         */
+        return null;
+    }
+}
---
+++ src/Tuple.java
@@ -0,0 +1,13 @@
+public class Tuple {
+
+    private double p;
+    private Degree degree;
+
+    public double getP() {
+        return p;
+    }
+
+    public Degree getDegree() {
+        return degree;
+    }
+}
