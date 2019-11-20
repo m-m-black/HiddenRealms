f32d11ddb53f6afc17508763c69e33e37bccd7f5
m-m-black <morganblack1988@gmail.com>
2019-11-20 16:27:11 +1100

    Created enum Key

---
+++ src/datastructures/Key.java
@@ -0,0 +1,16 @@
+package datastructures;
+
+public enum Key {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    Ab,
+    Bb,
+    Dd,
+    Eb,
+    Gb
+}
--- src/generative/ChordModel.java
+++ src/generative/ChordModel.java
@@ -1,8 +1,27 @@
 package generative;
 
+import datastructures.Mode;
+
 import java.util.ArrayList;
 
 public abstract class ChordModel {
 
+    private Mode mode; // -- This defines the available notes for the chord model --
+
+    public ChordModel() {
+        this.mode = null;
+    }
+
+    public ChordModel(Mode mode) {
+        this.mode = mode;
+    }
+
+    /*
+        Returns a list of integers representing MIDI notes
+     */
     public abstract ArrayList<Integer> nextChord();
+
+    public void setMode(Mode mode) {
+        this.mode = mode;
+    }
 }
